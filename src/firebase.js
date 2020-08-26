f17a1811572477c8141d7501fecc1cca3d8356bc
Somya Prajapati <somya2001p@gmail.com>
2020-08-26 09:32:54 +0530

    fits commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAtcff3aMIUmwWHaa9cX3i69PBHI-3kTNk",
+    authDomain: "whats-app-clone-1fb8b.firebaseapp.com",
+    databaseURL: "https://whats-app-clone-1fb8b.firebaseio.com",
+    projectId: "whats-app-clone-1fb8b",
+    storageBucket: "whats-app-clone-1fb8b.appspot.com",
+    messagingSenderId: "512255490558",
+    appId: "1:512255490558:web:b32c71383ecc6babc6e8af",
+    measurementId: "G-FSB7VNM7G0"
+  };
