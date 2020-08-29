a8ce5062b7ce3a4769200f10e4d06fa292244399
Somya Prajapati <64978837+somya51p@users.noreply.github.com>
2020-08-30 00:26:56 +0530

    making dynamic

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
@@ -6,8 +6,22 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
 import "./Sidebar.css";
 import SidebarChat from "./SidebarChat";
+import db from "./firebase";
 
 function Sidebar(){
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        db.collection("rooms").onSnapshot((snapshot)
+        => 
+            setRooms(
+                snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    data:doc.data(),
+                }))
+            )
+        );
+    }, []);
 
     return (
         <div className="sidebar">
@@ -34,17 +48,14 @@ function Sidebar(){
             </div>
             <div className="sidebar__chats">
                 <SidebarChat addNewChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
+                {rooms.map(room => (
+                    <SidebarChat key={room.id} id={room.id}
+                    name={room.data.name} />
+                ))}
                 
             </div>
-
-            <div className="sidebar__chats">
-
-            </div>
         </div>
-    )
+    );
 }
 
 export default Sidebar
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -21,7 +21,7 @@ function SidebarChat({ addNewChat }){
         <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className="sidebarChat__info">
-                <h2>Room Name</h2>
+                <h2>{name}</h2>
                 <p>Last message...</p>
             </div>
         </div>
